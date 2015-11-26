6f37fba09aac76e006a9d08273c7918db535d2ea
RgBerou <brotark@gmail.com>
2015-11-26 10:27:29 -0800

    With registration functioning and profile and dashboard ui only

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,17 +1,22 @@
-$(document).ready(function() {
-  var app = angular.module('proloops', []);
+(function() {
+    var app = angular.module('proLoop', []);
 
-  app.controller('ProloopsController', function() {
-    this.products = accounts;
-  });
+    app.controller('LoopController', function(){
+        this.product = pro;
+    });
 
-
-  var accounts = [{
-      name: 'Genaro C. Legaspi',
-      post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius nisl sed laoreet gravida. Duis elementum eleifend nibh, eu dictum ligula feugiat et. Proin luctus interdum commodo.",     
-      image: "image/gem-02.gif",
-  }];
-        
-      
-
+    var pro = [{
+        name: 'Mark Justin Tumulak',
+        location: 'Lapu-Lapu City',
+        post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas neque totam aliquam sequi aliquid, distinctio facere obcaecati illo eius, quis veniam fugit, quae delectus, quibusdam. Aut tempore excepturi amet fugiat!",
+        img:'/stylesheets/css/image/mjt.jpg'
+    },{
+        name: 'Genaro Legaspi',
+        post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas neque totam aliquam sequi aliquid, distinctio facere obcaecati illo eius, quis veniam fugit, quae delectus, quibusdam. Aut tempore excepturi amet fugiat!",
+        img:'/stylesheets/css/image/profpic.jpg'
+    },{
+        name: 'Argie Berou',
+        post: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas neque totam aliquam sequi aliquid, distinctio facere obcaecati illo eius, quis veniam fugit, quae delectus, quibusdam. Aut tempore excepturi amet fugiat!",
+        img:'/stylesheets/css/image/rg.jpg'
+    }];
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,67 @@
 var express = require('express');
 var router = express.Router();
-var  User=require('../models/user');
+var User=require('../models/user');
 
+var ET_email;
+var ET_pass;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Proloops' });
+    res.render('index', { title: 'Proloops' });
 });
 
 router.get('/login',function(req, res, next) {
-  res.render('template', { title: 'Sign up now' });
+    res.render('template', { title: 'Sign up now' });
 
 });
 
+router.get('/validate', function(req, res, next) {
 
-router.post('/login', function(req, res, next) {
-  var conf=req.body.confpassword;
-  var password=req.body.password;
-    if(conf==password){
-      var newUser=new User();
-      newUser.lastname=req.body.lastname;
-      newUser.firstname=req.body.firstname;
-      newUser.password=req.body.password;
-      newUser.email=req.body.email;
-      newUser.save(function(err){
+});
+router.get('/loginuser',function(req, res, next) {
+    ET_email=req.body.em;
+    ET_pass=req.body.pass;
+
+    User.find({"email":ET_email,"password":ET_pass},function(err,data){
         if(err){
-          throw err;
+            console.log("error");
+            res.redirect('/login');
+        }else{
+            res.redirect('/newsfeed');
         }
-      });
-      res.redirect("/login");
+    });
+
+});
+
+router.post('/signup', function(req, res, next) {
+    var conf=req.body.confpassword;
+    var password=req.body.password;
+    if(conf==password){
+        var newUser=new User();
+        newUser.lastname=req.body.lastname;
+        newUser.firstname=req.body.firstname;
+        newUser.password=req.body.password;
+        newUser.email=req.body.email;
+        newUser.save(function(err){
+            if(err){
+                throw err;
+            }
+        });
+        res.redirect("/login");
     }else{
-      res.redirect('/');
+        res.redirect('/');
     }
 });
 
 
 router.get('/:lastname/:firstname', function(req, res, next) {
-  res.render('dashboard');
+    res.render('dashboard');
+});
+router.get('/profile', function(req, res, next) {
+    res.render('profile');
 });
 
 router.get('/newsfeed', function(req, res, next) {
-  res.render('dashboard');
+    res.render('dashboard');
 });
 
 module.exports = router;

2980d83905248f69b184bb02e1dbcb908282df9c
RgBerou <brotark@gmail.com>
2015-12-03 15:24:14 -0800

    Trappings in registration added-unique email

--- routes/signup.js
+++ routes/signup.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var User=require('../models/user');
 var newUser;
 /* Post registration of user */
-router.post('/', function(req, res, next) {
+/*router.post('/', function(req, res, next) {
     var conf=req.body.confpassword;
     var password=req.body.password;
     if(conf==password){
@@ -24,14 +24,42 @@ router.post('/', function(req, res, next) {
     }else{
         res.redirect('/');
     }
+}); */
+
+router.post('/', function(req, res, next) {
+    var conf=req.body.confpassword;
+    var password=req.body.password;
+    if(conf==password){
+        newUser=new User();
+        newUser.lastname=req.body.lastname;
+        newUser.firstname=req.body.firstname;
+        newUser.password=req.body.password;
+        newUser.email=req.body.email;
+        newUser.username="";
+        newUser.type="member";
+        res.redirect("/signup/verify");
+    }else{
+        res.redirect('/');
+    }
+});
+
+router.get('/verify', function(req, res, next) {
+    User.findOne({email:newUser.email},function(err,result){
+        if(result==null){
+            newUser.save(function(err){
+                if(err){
+                    throw err;
+                }
+            });
+            res.redirect("/login");
+        }else{
+            res.render('login',{message:'',choice:2,warning:"Email is already in used"});
+        }
+    });
 });
 
 
 
 
 
-
 module.exports = router;
-/**
- * Created by Rg on 11/26/2015.
- */
